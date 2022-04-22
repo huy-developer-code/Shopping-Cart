3e214aa109f7eb2e223730d1b4598ad40cb6f650
huy-developer-code <voquanghuy862001@email.com>
2022-04-22 10:14:18 +0700

    edit database 1

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports={
-    database:"mongodb://localhost:27017/cmscart",
+    database:"mongodb+srv://voquanghuy:123@cluster0.0cygz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
 };
 
